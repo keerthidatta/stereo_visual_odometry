bcbd53c8116f288f6501860892837ff6c6269f8d
Keerthidatta <keerthidatta@gmail.com>
2020-10-10 00:22:27 +0200

    added build files

---
+++ include/stereo_visual_odometry/build/CMakeFiles/stereo_visual_odometry.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stereo_visual_odometry.dir/core/system.cpp.o"
+  "CMakeFiles/stereo_visual_odometry.dir/core/tracking.cpp.o"
+  "CMakeFiles/stereo_visual_odometry.dir/src/main.cpp.o"
+  "stereo_visual_odometry"
+  "stereo_visual_odometry.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stereo_visual_odometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
